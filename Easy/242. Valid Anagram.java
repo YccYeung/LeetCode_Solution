eb3a65ca10980f23015d41685e53df57c6ec3f64
IGoToSchoolByBus <70379183+YccYeung@users.noreply.github.com>
2024-01-04 17:02:36 -0600

    Update 242. Valid Anagram.java

--- Easy/242. Valid Anagram.java
+++ Easy/242. Valid Anagram.java
@@ -1,11 +1,15 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        // convert both String to char for sorting
         char[] charS = s.toCharArray();
         char[] charT = t.toCharArray();
+        // sort both char array
         Arrays.sort(charS);
         Arrays.sort(charT);
+        // convert char array back to String
         String newS = String.valueOf(charS);
         String newT = String.valueOf(charT);
+        // compare the content of both String
         boolean toReturn = newS.equals(newT);
         return toReturn;
     }
