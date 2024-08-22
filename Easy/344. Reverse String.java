d82352c707d99e6d9c701c008096d26538aed4ae
IGoToSchoolByBus <70379183+YccYeung@users.noreply.github.com>
2024-08-21 23:12:02 -0500

    Update 344. Reverse String.java

--- Easy/344. Reverse String.java
+++ Easy/344. Reverse String.java
@@ -1,15 +1,18 @@
 class Solution {
     public void reverseString(char[] s) {
+        // set up two pointers
         int x = 0;
         int y = s.length - 1;
 
         for (int i = 0; i < s.length; i++) {
             if (x >= y) {break;}
             else {
+                // perform a swap operation
                 char temp;
                 temp = s[x];
                 s[x] = s[y];
                 s[y] = temp;
+                // update two pointers
                 x++;
                 y--;
             }
